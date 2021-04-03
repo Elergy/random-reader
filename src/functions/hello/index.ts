e07c6cc0027e4b250172d032889ae3a8a28b2d49
elergy <ponomarev.e.s@gmail.com>
2021-04-03 18:17:39 +0100

    initial lint

--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,18 +1,19 @@
-import schema from './schema';
+import schema from "./schema";
 
 export default {
-  handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
-  events: [
-    {
-      http: {
-        method: 'post',
-        path: 'hello',
-        request: {
-          schema: {
-            'application/json': schema
-          }
-        }
-      }
-    }
-  ]
-}
+    handler: `${__dirname.split(process.cwd())[1].substring(1)}/handler.main`,
+    memorySize: 128,
+    events: [
+        {
+            http: {
+                method: "post",
+                path: "hello",
+                request: {
+                    schema: {
+                        "application/json": schema,
+                    },
+                },
+            },
+        },
+    ],
+};
